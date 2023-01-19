4eb0ee30e70e9bf941af1fc45668c7f9a20ab6ad
sdilruba <88456908+sdilruba@users.noreply.github.com>
2023-01-19 15:57:33 +0100

    reformatted code

--- tests/pages/HomePage.js
+++ tests/pages/HomePage.js
@@ -1,8 +1,9 @@
-class HomePage{
+class HomePage {
 
     pageURL = 'https://www.share-now.com/de/en/';
-    acceptCookies =  '[data-testid=uc-accept-all-button]';
+    acceptCookies = '[data-testid=uc-accept-all-button]';
     signUpButton = '.primary-navbar__actions #reggie-link-register-now';
 
 }
+
 export default HomePage;
--- tests/shareNow.spec.js
+++ tests/shareNow.spec.js
@@ -1,4 +1,4 @@
-import {test, expect} from '@playwright/test';
+import {expect, test} from '@playwright/test';
 import PersonalDataPage from '../tests/pages/PersonalDataPage'; // your Path of the page object class
 import HomePage from "./pages/HomePage";
 
