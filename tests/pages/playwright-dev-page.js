5b2e36074d88e77c6b4760cbd16daf3a9b500205
sdilruba <88456908+sdilruba@users.noreply.github.com>
2023-01-26 14:53:07 +0100

    implemented Page Object base page

---
+++ tests/pages/playwright-dev-page.js
@@ -0,0 +1,33 @@
+// playwright-dev-page.js
+const {expect} = require('@playwright/test');
+
+exports.PlaywrightDevPage = class PlaywrightDevPage {
+
+    /**
+     * @param {import('@playwright/test').Page} page
+     */
+    constructor(page) {
+        this.page = page;
+        this.drivingLocation = page.locator(`select[name=drivingLocation]`);
+        this.emailInput = page.locator(`input[name=email]`);
+        this.passwordInput = page.locator('input[name=password]');
+        this.pinInput = page.locator('input[name=pin]');
+        this.salutationInput = page.locator('select[name=salutation]');
+        this.firstNameInput = page.locator('input[name=firstName]');
+        this.lastNameInput = page.locator('input[name=lastName]');
+        this.birthDayInput = page.locator('#camelot-select-birth-date-day');
+        this.birthMonthInput = page.locator('#camelot-select-birth-date-month');
+        this.birthYearInput = page.locator('#camelot-select-birth-date-year');
+        this.birthPlaceInput = page.locator('input[name=birthPlace]');
+        this.addressStreetInput = page.locator('input[name=addressStreet]');
+        this.addressZipcodeInput = page.locator('input[name=addressZipCode]');
+        this.addressCityInput = page.locator('input[name=addressCity]');
+        this.mobilePhoneInput = page.locator('input[name=mobilePhone]');
+        this.registrationCheckboxes = page.locator('.checkbox [type=checkbox]');
+        this.registrationButton = page.locator('#registration-save-button');
+    }
+
+    async goto() {
+        await this.page.goto('https://www.share-now.com/de/en/berlin/registration/personal-data/');
+    }
+}
