fb04a0a2332b1be0f06b5cbad617ad29bef821ec
sdilruba <88456908+sdilruba@users.noreply.github.com>
2023-01-31 17:29:35 +0100

    created pages

--- tests/check24.spec.js
+++ tests/check24.spec.js
@@ -1,14 +1,19 @@
 import {expect, test} from '@playwright/test';
+import HomePage from "./check24Pages/HomePage";
+import ListingPage from "./check24Pages/ListingPage";
 
 test('playstation5goToPDP', async ({page}) => {
 
-    await page.goto('https://www.check24.de/');
-    await page.locator('.c24-cookie-consent-button').nth(1).click();
-    await page.locator('input#c24-search-header').type('ps5');
-    await page.keyboard.press('Enter');
-    await page.locator('.category-select-button:first-child').click();
-    await page.locator('.check-box-option__box').nth(2).click();
-    await page.locator('.result-item-tile').nth(0).click();
+    const homePage = new HomePage();
+    const listingPage = new ListingPage();
+
+    await page.goto(homePage.pageURL);
+    await page.locator(homePage.acceptCookies).nth(1).click();
+    await page.locator(homePage.searchField).type('ps5');
+    await page.locator(homePage.searchButton).click();
+    await page.locator(listingPage.firstCategory).click();
+    await page.locator(listingPage.checkBoxFilters).nth(2).click();
+    await page.locator(listingPage.listingTiles).nth(0).click();
     await expect(page).toHaveURL(/sony-playstation/);
 
 });
