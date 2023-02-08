bb4bc3ba706ee5160d24419ec3851441e44415b0
sdilruba <88456908+sdilruba@users.noreply.github.com>
2023-02-08 10:07:00 +0100

    first draft with multiple waits and HomePage.js

---
+++ tests/check24Login.spec.js
@@ -0,0 +1,47 @@
+import {expect, test} from '@playwright/test';
+import HomePage from "./check24Pages/HomePage";
+
+test('playstation5goToPDP', async ({page}) => {
+
+    const homePage = new HomePage();
+
+    await page.goto(homePage.pageURL);
+    await page.locator(homePage.acceptCookies).nth(1).click();
+    await page.hover(homePage.myAccount);
+    await page.locator(homePage.signIn).click();
+
+    await new Promise(r => setTimeout(r, 5000));
+
+    await page.locator(homePage.register).click();
+
+    await new Promise(r => setTimeout(r, 5000));
+
+    const randomEmail = Math.floor(Math.random() * 1000000000) + '@dummyaddress.com';
+    await page.locator(homePage.emailOrPhoneNumberInput).type(randomEmail);
+
+    await new Promise(r => setTimeout(r, 5000));
+   // await page.keyboard.press('Enter');
+
+    await page.locator(homePage.continueButton).click();
+
+    await new Promise(r => setTimeout(r, 5000));
+
+    await page.locator(homePage.firstNameInput).fill('alo');
+
+    await new Promise(r => setTimeout(r, 5000));
+
+    await page.locator(homePage.lastNameInput).fill('aloalo');
+
+    await new Promise(r => setTimeout(r, 5000));
+
+    await page.locator(homePage.passwordInput).fill('686115Hc.');
+
+    await new Promise(r => setTimeout(r, 5000));
+
+    await page.locator(homePage.repeatedPassword).fill('686115Hc.');
+
+    await new Promise(r => setTimeout(r, 5000));
+
+    await page.locator(homePage.submitButton).click();
+
+});
--- tests/check24Pages/HomePage.js
+++ tests/check24Pages/HomePage.js
@@ -4,6 +4,26 @@ class HomePage {
     acceptCookies ='.c24-cookie-consent-button';
     searchButton = '.c24-search-button';
     searchField = 'input#c24-search-header';
+
+    //Nav-bar
+    myAccount = '.c24-customer-hover-wrapper';
+
+    //My Account Pop-Up Screen
+    signIn = '#c24-meinkonto-anmelden';
+    register = '.c24-uli-cl-r-start-trigger';
+    emailOrPhoneNumberInput = '#cl_login';
+    continueButton = 'c24-uli-login-btn';
+
+    //Register screen
+    firstNameInput = '#cl_ul_firstname';
+    lastNameInput = '#cl_ul_lastname';
+    passwordInput = '#cl_pw_register';
+    repeatedPassword = '#cl_ul_pw_register_repeat';
+    submitButton = '#c24-uli-register-btn';
+
+    //Registration Confirmation Screen
+    confirmationCodeTitle = '.c24-uli-title';
+
 }
 
 export default HomePage;
