e88d2cc85895a0ead506c1966673a3a3ba8546f6
sdilruba <88456908+sdilruba@users.noreply.github.com>
2023-02-23 13:49:24 +0100

    used UserTestData in check24Login.spec.js

--- tests/check24Login.spec.js
+++ tests/check24Login.spec.js
@@ -1,25 +1,26 @@
 import {expect, test} from '@playwright/test';
 import HomePage from "./check24Pages/HomePage";
 import RegistrationFrame from "./check24Pages/RegistrationFrame";
+import {UserTestData} from "../utils/UserTestData";
 
-test('Create an account untill the confirmation code page',
+test('Create an account until the confirmation code page',
     async ({page}) => {
 
         const homePage = new HomePage();
         const registrationFrame = new RegistrationFrame();
+        const testUser = new UserTestData();
 
         await page.goto(homePage.pageURL);
         await page.locator(homePage.acceptCookies).nth(1).click();
         await page.hover(homePage.myAccount);
         await page.locator(registrationFrame.signIn).click();
         await page.frameLocator(registrationFrame.registrationFrame).locator(registrationFrame.createAccountButton).click();
-        const randomEmail = Math.floor(Math.random() * 1000000000) + '@dummyaddress.com';
-        await page.frameLocator(registrationFrame.registrationFrame).locator(registrationFrame.emailOrPhoneNumberInput).type(randomEmail);
+        await page.frameLocator(registrationFrame.registrationFrame).locator(registrationFrame.emailOrPhoneNumberInput).type(testUser.email);
         await page.keyboard.press('Enter');
-        await page.frameLocator(registrationFrame.registrationFrame).locator(registrationFrame.firstNameInput).fill('alo');
-        await page.frameLocator(registrationFrame.registrationFrame).locator(registrationFrame.lastNameInput).fill('aloalo');
-        await page.frameLocator(registrationFrame.registrationFrame).locator(registrationFrame.passwordInput).fill('686115Hc.');
-        await page.frameLocator(registrationFrame.registrationFrame).locator(registrationFrame.repeatedPassword).fill('686115Hc.');
+        await page.frameLocator(registrationFrame.registrationFrame).locator(registrationFrame.firstNameInput).fill(testUser.firstName);
+        await page.frameLocator(registrationFrame.registrationFrame).locator(registrationFrame.lastNameInput).fill(testUser.lastName);
+        await page.frameLocator(registrationFrame.registrationFrame).locator(registrationFrame.passwordInput).fill(testUser.password);
+        await page.frameLocator(registrationFrame.registrationFrame).locator(registrationFrame.repeatedPassword).fill(testUser.password);
         await page.frameLocator(registrationFrame.registrationFrame).locator(registrationFrame.submitButton).click();
         await expect(page.frameLocator(registrationFrame.registrationFrame).locator(registrationFrame.changeEmailButton)).toBeVisible();
     });
